2e83c43f1baee4b3b3c85c3a0ab1ae0239e00125
saaj <harsaajbhatia@gmail.com>
2024-11-28 12:43:19 -0800

    fix tests

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tests/ViperTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tests/ViperTest.java
@@ -1,38 +1,80 @@
-package org.firstinspires.ftc.teamcode;
+/* Copyright (c) 2017 FIRST. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted (subject to the limitations in the disclaimer below) provided that
+ * the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this list
+ * of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice, this
+ * list of conditions and the following disclaimer in the documentation and/or
+ * other materials provided with the distribution.
+ *
+ * Neither the name of FIRST nor the names of its contributors may be used to endorse or
+ * promote products derived from this software without specific prior written permission.
+ *
+ * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
+ * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+package org.firstinspires.ftc.teamcode.tests;
+
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.util.Range;
 
-@TeleOp(name="Servo Set to Zero")
-public class ViperTest extends LinearOpMode {
-    private Servo myServo;
-    private Servo myServo2;
+import org.firstinspires.ftc.teamcode.Arm;
+
+/*
+ * This file contains an example of an iterative (Non-Linear) "OpMode".
+ * An OpMode is a 'program' that runs in either the autonomous or the teleop period of an FTC match.
+ * The names of OpModes appear on the menu of the FTC Driver Station.
+ * When a selection is made from the menu, the corresponding OpMode
+ * class is instantiated on the Robot Controller and executed.
+ *
+ * This particular OpMode just executes a basic Tank Drive Teleop for a two wheeled robot
+ * It includes all the skeletal structure that all iterative OpModes contain.
+ *
+ * Use Android Studio to Copy this Class, and Paste it into your team's code folder with a new name.
+ * Remove or comment out the @Disabled line to add this OpMode to the Driver Station OpMode list
+ */
+
+@TeleOp(name="ViperTestMode", group="Iterative OpMode")
+public class ViperTest extends OpMode
+{
+
+    private Arm arm;
 
     @Override
-    public void runOpMode() {
-        // Initialize the servo
-        myServo = hardwareMap.get(Servo.class, "SERVO1");
-        myServo2 = hardwareMap.get(Servo.class, "SERVO2");
-
-        // Wait for the start button to be pressed
-        waitForStart();
-
-        // Set the servo position to 0
-        myServo.setPosition(0.0);
-        myServo2.setPosition(0.0);
-
-        if (gamepad1.a) {
-            myServo.setPosition(1.0);
-            myServo2.setPosition(1.0);
-        }
-
-
-        // Keep the op mode running
-        while (opModeIsActive()) {
-            telemetry.addData("Servo Position", myServo.getPosition());
-            telemetry.addData("Servo Position 2", myServo2.getPosition());
-            telemetry.update();
-        }
+    public void init() {
+        arm = new Arm(hardwareMap);
     }
+
+    @Override
+    public void loop() {
+        arm.moveViperSlides(gamepad1.left_trigger - gamepad1.right_trigger);
+
+//        if (gamepad1.left_bumper) {
+//            arm.raise(0.2);
+//        } else if (gamepad1.right_bumper) {
+//            arm.lower(0.2);
+//        } else {
+//            arm.stopPivot();
+//        }
+    }
+
 }
